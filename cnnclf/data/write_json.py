e5e9b466c526c0c19c083e83b3171568e9817054
antonilo <aloquercio@student.ethz.ch>
2016-02-14 19:53:03 +0100

    json dumper added

---
+++ cnnclf/data/write_json.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import os
+import numpy as np
+
+# initialize file
+train =  open("./meta/train.json", 'w')
+test = open("./meta/test.json", 'w')
+
+train.write("{\n")
+test.write("{\n")
+
+
+for root, dirs, files in os.walk("./img"):
+    for j,name in enumerate(dirs):
+        f_path = os.path.join(root,name)
+        train.write('"{}": [\n'.format(name))
+        test.write('"{}": [\n'.format(name))
+        for _,__, files in os.walk(f_path):
+            files = np.array(files)
+            np.random.shuffle(files)
+            N = len(files)
+            thresh = N/5
+            for i, path in enumerate(files):
+                if (i < thresh ):
+                    if i == thresh -1:
+                        test.write('"{}/{}"\n'.format(name,path))
+                    else:
+                        test.write('"{}/{}",\n'.format(name,path))
+                elif ( i> thresh and i != N-1):
+                    train.write('"{}/{}",\n'.format(name,path))
+                else:
+                    train.write('"{}/{}"\n'.format(name,path))
+            train.write(']')
+            test.write(']')
+            if j == 0:
+                train.write(',')
+                test.write(',')
+            train.write('\n')
+            test.write('\n')
+test.write('}')
+train.write('}')
+
+
